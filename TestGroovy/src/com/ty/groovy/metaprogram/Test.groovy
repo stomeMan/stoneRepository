bc0979452f4c5eb21dc6b9114880855d38cb407a
gaohailiang <gaohailiang@tv189.com>
2014-06-13 17:10:20 +0800

    add

---
+++ TestGroovy/src/com/ty/groovy/metaprogram/Test.groovy
@@ -0,0 +1,25 @@
+package com.ty.groovy.metaprogram
+
+import com.ty.groovy.helloworld.HelloWorld;
+
+//class Test {
+//
+//	static main(args) {
+//	
+//	}
+//	Integer.metaClass.pounce{  
+//		println delegate
+//		def s = "Boing!"
+//		delegate.upto(delegate+1){ s += " boing!" }
+//		s + "!"
+//	}
+//	print  3.pounce() == "Boing! boing! boing!!"
+//	
+//}assert
+HelloWorld.metaClass.printname{
+	println name;
+} 
+
+HelloWorld h=new HelloWorld();
+h.setName("hh")
+h.printname();
