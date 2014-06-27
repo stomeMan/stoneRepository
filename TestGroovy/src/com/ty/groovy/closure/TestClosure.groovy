51ff6a48c10c16a6dfb27a3d4332478fb968e853
gaohailiang <gaohailiang@tv189.com>
2014-06-27 10:56:41 +0800

    add

--- TestGroovy/src/com/ty/groovy/closure/TestClosure.groovy
+++ TestGroovy/src/com/ty/groovy/closure/TestClosure.groovy
@@ -3,7 +3,8 @@ package com.ty.groovy.closure
 //class TestClosure {
 //
 //}
-def c = {  println it }
-c.call('Hello Closure');
-def cls = 'hello world'.&length; 
-println cls()
+//def c = {  println it }
+//c.call('Hello Closure');
+//def cls = 'hello world'.&length; 
+//println cls()
+print "ddd\nddd"
--- TestGroovy/src/com/ty/java/testmap/TestHashMap.java
+++ TestGroovy/src/com/ty/java/testmap/TestHashMap.java
@@ -1,14 +1,19 @@
 package com.ty.java.testmap;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class TestHashMap {
 	public static void main(String[] args) {
-		Map<String,Object> map=new HashMap<String, Object>();
+		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("id", 1);
 		map.put("name", "jack");
 		map.put("age", 30);
-		System.out.println(map.get("id"));;
+		for (Iterator<Entry<String, Object>> it = map.entrySet().iterator(); it.hasNext();) {
+			Entry<String, Object> e=it.next();
+			System.out.println("key:"+e.getKey()+"   "+"value:"+e.getValue());
+		}
 	}
 }
